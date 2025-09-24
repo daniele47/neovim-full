d85004b5360cff9baccb2b519eb11e671d3b8ffa
- <->
2025-09-24 06:40:19 +0200

    updated run to allow binding directories (with strong checks to avoid unsafe directories)

--- run.sh
+++ run.sh
@@ -1,9 +1,32 @@
 #!/bin/bash
 
+set -e
+
+UNSAFE_DIRS=(
+    "$HOME"
+    "$HOME/.config"
+    "$HOME/.local"
+    "$HOME/.local/share"
+    "$HOME/.local/state"
+    "$HOME/.var"
+    "$HOME/.var/app"
+)
+DIR_PATH="$(realpath "$1")"
+
+# checks on string to mount
+if [ ! -d "$DIR_PATH" ] || [ ! -w "$DIR_PATH" ] || [ ! -O "$DIR_PATH" ]; then
+    echo "invalid directory: '$DIR_PATH'"
+    exit 1
+fi
+for UNSAFE_DIR in "${UNSAFE_DIRS[@]}"; do 
+    [[ "$DIR_PATH" == "$UNSAFE_DIR" ]] && echo "'$UNSAFE_DIR' is an unsafe directory to mount!" && exit 1
+done
+
 podman run -it --rm \
     --init \
     -e "TZ=$(timedatectl show --property=Timezone --value)" \
     --detach-keys="" \
-    -v neovim-data:/data \
+    --security-opt label=type:container_runtime_t \
+    -v "$DIR_PATH":/data \
     -w /data \
     ghcr.io/daniele47/neovim bash
