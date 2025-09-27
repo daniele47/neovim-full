a6cbb198881c9f25cfec899a27a1f3f12d479e8a
- <->
2025-09-27 23:01:32 +0200

    add back mason lspconfig

--- image/nvim/lua/plugins/lsp_mason.lua
+++ image/nvim/lua/plugins/lsp_mason.lua
@@ -1,4 +1,9 @@
 return {
-	{ "neovim/nvim-lspconfig" },
-	{ "mason-org/mason.nvim", opts = {} },
+	"mason-org/mason-lspconfig.nvim",
+	opts = {},
+	event = "VeryLazy",
+	dependencies = {
+		{ "neovim/nvim-lspconfig" },
+		{ "mason-org/mason.nvim", opts = {} },
+	},
 }
